c6e1e7237a31d807739fc645f7719b177bfe0e00
Gorbachev Egor <7gorbachevm@gmail.com>
2024-06-26 13:00:05 +0700

    GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,24 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google"
+import { Inter as FontSans } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
-import {cn} from "@/lib/utils";
+import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
   title: "MemoCard",
-  keywords: ["anki alternative", "flashcard app", "flashcards ai", "create ai flashcards"],
-  description: "Improve your memory with spaced repetition. Learn languages, history or other subjects with the proven flashcard method.",
+  keywords: [
+    "anki alternative",
+    "flashcard app",
+    "flashcards ai",
+    "create ai flashcards",
+  ],
+  description:
+    "Improve your memory with spaced repetition. Learn languages, history or other subjects with the proven flashcard method.",
 };
 
 export default function RootLayout({
@@ -21,10 +28,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body  className={cn(
+      {process.env.NODE_ENV === "production" && (
+        <GoogleAnalytics gaId="G-Z45JH1JS3K" />
+      )}
+      <body
+        className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-      )}>{children}</body>
+          fontSans.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
